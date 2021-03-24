42987f2916352efe4ab298f9ad0ce45bde2e4615
lixy <lixy@ansosz.cn>
2021-03-24 19:47:49 +0800

    修复格式问题

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/index.sass'
 
 Vue.use(Element)
